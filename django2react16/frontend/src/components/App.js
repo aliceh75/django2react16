2ea41ec50322f2346b42bac542a71a8f1e59e303
Alice Heaton <aliceh@aptivate.org>
2019-04-16 19:03:14 +0100

    Add linting and PropTypes

--- django2react16/frontend/src/components/App.js
+++ django2react16/frontend/src/components/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import AppContent from "./AppContent";
 
 class App extends React.Component {
-    render() {
-          return <div>
-              <h1>Django2React16!</h1>
-              <AppContent />
-            </div>;
-        }
+  render() {
+    return <div>
+      <h1>Django2React16!</h1>
+      <AppContent counter={1} />
+    </div>;
+  }
 }
 
 export default App;
--- django2react16/frontend/src/components/AppContent.js
+++ django2react16/frontend/src/components/AppContent.js
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class AppContent extends React.Component {
+  static propTypes = {
+    counter: PropTypes.number.isRequired
+  };
+
   render() {
-    return <div>Hello World</div>;
+    return <div>Hello World: {this.props.counter}</div>;
   }
 }
 
